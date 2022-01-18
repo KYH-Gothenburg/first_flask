0c30bc598c2c801ee2c1f8bcabedb2d1ae485614
Joakim Wassberg <joakim@arthead.se>
2022-01-18 16:05:21 +0100

    Finished signup

--- blueprints/open/__init__.py
+++ blueprints/open/__init__.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from werkzeug.security import generate_password_hash
+from models import User
 
 # Create a blueprint object that can be used as an app object for this blueprint
 bp_open = Blueprint('bp_open', __name__)
@@ -21,5 +23,22 @@ def signup_get():
 
 @bp_open.post('/signup')
 def signup_post():
+    name = request.form['name']
+    email = request.form.get('email')
+    password = request.form['password']
+    hashed_password = generate_password_hash(password, method='sha256')
+
+    # Check if user with this password exists in the database
+    user = User.query.filter_by(email=email).first()  # First will give us an object if user exist, or None if not
+    if user:
+        # If user is not none, then a user with this email exists in the database
+        flash("Email address is already in use")
+        return redirect(url_for('bp_open.signup_get'))
+
+    new_user = User(name=name, email=email, password=hashed_password)
+
+    from app import db
+    db.session.add(new_user)
+    db.session.commit()
 
     return redirect(url_for('bp_open.login_get'))
