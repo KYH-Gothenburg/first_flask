1b95fcc54a3231c9b37c6a84d76eb74d8f65bdab
Joakim Wassberg <joakim@arthead.se>
2022-01-18 14:07:42 +0100

    Started work on DB

--- app.py
+++ app.py
@@ -1,25 +1,33 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-username = 'Bob'
-friends = ['Alice', 'Carl', 'Dina', 'Eric', 'Poppe', 'Lisa', 'Ulla']
-
-
-@app.get('/')
-def index():
-    return render_template("index.html")
+db = SQLAlchemy()
 
 
-@app.get('/user')
-def user_get():
-    return render_template("user.html", name=username, friends=friends)
+# We will use a factory function to avoid cyclic imports
+def create_app():
+    app = Flask(__name__)
+    # Many parts of flask will require use to use a secret key so we create one
+    app.config['SECRET_KEY'] = '123secret'
+    # Will configure SQLAlchemy to use SQLite and the file db.sqlite
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+
+    # Init the SQLAlchemy object with our app object
+    db.init_app(app)
+
+    from blueprints.open import bp_open
+    app.register_blueprint(bp_open)
 
 
-@app.post('/user')
-def user_post():
-    friend = request.form['friend_name']
-    friends.append(friend)
-    return redirect(url_for('user_get'))
+    @app.get('/user')
+    def user_get():
+        return render_template("user.html")
+
+    @app.post('/user')
+    def user_post():
+        return redirect(url_for('user_get'))
+
+    return app
 
 
 
---
+++ blueprints/open/__init__.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, render_template
+
+bp_open = Blueprint('bp_open', __name__)
+
+
+@bp_open.get('/')
+def index():
+    return render_template("index.html")
