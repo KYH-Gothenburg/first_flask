f2d14d385f6ddf398965218286826fcaa300a585
Joakim Wassberg <joakim@arthead.se>
2022-01-20 10:39:12 +0100

    Started login code

--- blueprints/open/__init__.py
+++ blueprints/open/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
-from werkzeug.security import generate_password_hash
 from models import User
+from passlib.hash import argon2
 
 # Create a blueprint object that can be used as an app object for this blueprint
 bp_open = Blueprint('bp_open', __name__)
@@ -16,6 +16,13 @@ def login_get():
     return render_template('login.html')
 
 
+@bp_open.post('/login')
+def login_post():
+    email = request.form['email']
+    password = request.form['password']
+    return redirect(url_for('bp_open.index'))
+
+
 @bp_open.get('/signup')
 def signup_get():
     return render_template('signup.html')
@@ -26,7 +33,7 @@ def signup_post():
     name = request.form['name']
     email = request.form.get('email')
     password = request.form['password']
-    hashed_password = generate_password_hash(password, method='sha256')
+    hashed_password = argon2.using(rounds=10).hash(password)
 
     # Check if user with this password exists in the database
     user = User.query.filter_by(email=email).first()  # First will give us an object if user exist, or None if not
